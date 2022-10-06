7da71728e810fefd6a45870c846e0e09e6c3d398
Kamil Mielnik <kamil.adam.mielnik@gmail.com>
2022-10-06 22:21:36 +0700

    Introduce findIcons

---
+++ src/findIcons.ts
@@ -0,0 +1,44 @@
+import probeImageSize from 'probe-image-size';
+
+import { getCandidateUrls, getHtmlCandidateUrls, sortIcons } from './lib';
+import { Options, Icon } from './types';
+
+/**
+ * Tries to find all icons that represent given URL.
+ * Icons are sorted descending (best first).
+ * Favors vector images, square images, and then large images.
+ * It never throws.
+ */
+const findIcons = async (url: string, options: Options = {}): Promise<Icon[]> => {
+  const htmlCandidateUrls = getHtmlCandidateUrls(url);
+  const htmlCandidateUrlsStack = [...htmlCandidateUrls].reverse();
+  const icons: Icon[] = [];
+  let htmlCandidateUrl: string | undefined;
+
+  while ((htmlCandidateUrl = htmlCandidateUrlsStack.pop())) {
+    try {
+      const candidateUrls = await getCandidateUrls(htmlCandidateUrl, options);
+
+      for (const candidateUrl of candidateUrls) {
+        try {
+          const info = await probeImageSize(candidateUrl);
+          const icon = { url: candidateUrl, info };
+          icons.push(icon);
+        } catch {
+          // skip this candidateUrl
+        }
+      }
+
+      // do not try another htmlCandidateUrl website if some icon has been found
+      if (icons.length > 0) {
+        return sortIcons(icons);
+      }
+    } catch {
+      // skip this htmlCandidateUrl
+    }
+  }
+
+  return [];
+};
+
+export default findIcons;
