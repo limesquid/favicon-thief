25b305a6d898bc290529e1765538782c38f3a492
Kamil Mielnik <kamil.adam.mielnik@gmail.com>
2022-10-06 21:42:26 +0700

    Prettier

--- src/lib/isGoodIcon.ts
+++ src/lib/isGoodIcon.ts
@@ -3,8 +3,6 @@ import { ProbeResult } from 'probe-image-size';
 import { DEFAULT_MIN_SIZE, DEFAULT_PREFER_SQUARE } from '../constants';
 import type { Options } from '../types';
 
-
-
 const isGoodIcon = (result: ProbeResult, options: Options = {}): boolean => {
   const { minSize = DEFAULT_MIN_SIZE, preferSquare = DEFAULT_PREFER_SQUARE } = options;
   const { height, width } = result;
