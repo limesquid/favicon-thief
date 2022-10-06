3ccfa67be9300f624bf194960284536c46a5abf6
Kamil Mielnik <kamil.adam.mielnik@gmail.com>
2022-10-06 22:34:36 +0700

    Export findIcons

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
 export { default as findIcon } from './findIcon';
+export { default as findIcons } from './findIcons';
 export * from './constants';
 export * from './lib';
 export * from './types';
