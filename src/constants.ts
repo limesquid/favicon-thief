b41598faa01d1ff413f9646c1f36580c37853bd1
Kamil Mielnik <kamil.adam.mielnik@gmail.com>
2022-10-06 21:36:41 +0700

    Update default min size

--- src/constants.ts
+++ src/constants.ts
@@ -1,7 +1,7 @@
 export const ANY_SIZE = 'ANY';
 
-export const DEFAULT_PREFER_SQUARE = true;
+export const DEFAULT_MIN_SIZE = 256 * 256;
 
-export const DEFAULT_MIN_SIZE = 128 * 128;
+export const DEFAULT_PREFER_SQUARE = true;
 
 export const DEFAULT_USER_AGENT = 'Favicon Bot (https://www.npmjs.com/package/favicon-thief)';
