516635c1be94f2e35e6ae4630fca9216f837f033
Kamil Mielnik <kamil.adam.mielnik@gmail.com>
2022-12-05 21:11:07 +0700

    Add a test checking top 500 websites

---
+++ tests/index.js
@@ -0,0 +1,28 @@
+const download = require('download');
+
+const top500 = require('./top-500');
+
+const { findIcon } = require('../build');
+
+const MIME_TO_EXTENSION = {
+  'image/png': '.png',
+  'image/x-icon': '.ico',
+  'image/jpg': '.jpg',
+  'image/jpeg': '.jpeg',
+  'image/bmp': '.bmp',
+  'image/svg+xml': '.svg',
+};
+
+const run = async () => {
+  for (const website of top500) {
+    const url = 'https://' + website;
+    console.log('Checking', url);
+    const icon = await findIcon(url);
+    console.log(icon.url, icon.mime);
+    // const extension = MIME_TO_EXTENSION[icon.mime];
+    // const filename = website + extension;
+    // download(icon.url, `download/${filename}`);
+  }
+};
+
+run();
