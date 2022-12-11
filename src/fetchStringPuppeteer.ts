0edcadade359ed88f5964d95b3b98bb2ddd0d9af
Kamil Mielnik <kamil.adam.mielnik@gmail.com>
2022-12-11 22:49:16 +0700

    Move DEFAULT_USER_AGENT to constants

--- src/fetchStringPuppeteer.ts
+++ src/fetchStringPuppeteer.ts
@@ -1,10 +1,7 @@
 import puppeteer, { Browser } from 'puppeteer';
 
+import { DEFAULT_USER_AGENT } from './constants';
 import { StringResponse } from './types';
-
-// prevent to redirect to the mobile version of a website
-const DEFAULT_USER_AGENT =
-  'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/108.0.0.0 Safari/537.36';
 
 const fetchStringPuppeteer = async (
   url: string,
