1aebaafb61837cdecd8de9773b1a0640b77ac8ad
Kamil Mielnik <kamil.adam.mielnik@gmail.com>
2022-12-11 13:05:23 +0700

    Remove findIcon & findIcons

--- src/constants.ts
+++ src/constants.ts
@@ -1,3 +1,1 @@
 export const ANY_SIZE = 'any';
-
-// export const DEFAULT_MIN_SIZE = 256 * 256;
--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,4 @@
 export { default as findFaviconLinks } from './findFaviconLinks';
-export { default as findIcon } from './findIcon';
-export { default as findIcons } from './findIcons';
 export { default as fetchFaviconLinks } from './fetchFaviconLinks';
 export { default as fetchStringWithNodeFetch } from './fetchStringWithNodeFetch';
 export { default as fetchStringWithPuppeteer } from './fetchStringWithPuppeteer';
