90e1872e61e1f9a8ff13686b4f569f870a426a3c
Kamil Mielnik <kamil.adam.mielnik@gmail.com>
2022-12-11 13:04:00 +0700

    Fix build

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,9 @@
 export { default as findFaviconLinks } from './findFaviconLinks';
 export { default as findIcon } from './findIcon';
 export { default as findIcons } from './findIcons';
-export { default as getStringWithNodeFetch } from './getStringWithNodeFetch';
+export { default as fetchFaviconLinks } from './fetchFaviconLinks';
+export { default as fetchStringWithNodeFetch } from './fetchStringWithNodeFetch';
+export { default as fetchStringWithPuppeteer } from './fetchStringWithPuppeteer';
 export * from './constants';
 export * from './lib';
 export * from './types';
