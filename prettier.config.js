d1bf4bb534b0bee397e930759ba24b736b03d988
Kamil Mielnik <kamil@kamilmielnik.com>
2023-01-25 14:02:22 +0700

    Update deprecated option in prettier config

--- prettier.config.js
+++ prettier.config.js
@@ -5,7 +5,7 @@ module.exports = {
   semi: true,
   singleQuote: true,
   bracketSpacing: true,
-  jsxBracketSameLine: false,
+  bracketSameLine: false,
   arrowParens: 'always',
   printWidth: 100,
 };
